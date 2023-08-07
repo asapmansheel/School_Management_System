1b4a604383e626c55a76224f790927143c725d3f
Mansheel Randhawa <mansheelrandhawa2001@gmail.com>
2023-08-07 14:19:53 +0800

    Created Teacher class

--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,34 @@
 public class Teacher {
   
+  private int id;
+  private String name;
+  private double salary;
+
+   // Constructor
+  public Teacher(int id, String name, double salary) {
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+  }
+
+  // Getters and setters
+  // We are not going to alter id and name of student
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
+
+  
+  
 }
